2a20519cf998269e1102de283405e3c263f5968a
howardbdev <howarddevennish@gmail.com>
2019-08-02 15:48:43 -0400

    connect Animals to Redux

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import AnimalCard from './AnimalCard'
+import {connect} from 'react-redux'
 
 const Animals = ({ animals }) => (
-  animals.map(a => <AnimalCard key={a.id} animal={a}/>)
+  animals ? animals.map(a => <AnimalCard key={a.id} animal={a}/>) : null
 );
 
-export default Animals;
+const mapStateToProps = ({ animals }) => {
+  return ({
+    animals
+  })
+}
+export default connect(mapStateToProps)(Animals);
