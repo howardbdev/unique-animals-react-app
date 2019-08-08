57001ff42c5ea507b0f2c837b17c1c887f4aef9a
howardbdev <howarddevennish@gmail.com>
2019-08-08 12:37:01 -0400

    add some comments around redux

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import CenterContainer from "./components/CenterContainer"
 import { connect } from 'react-redux'
 import { getAnimals } from './actions/animals'
 
+// I want to be able to add a new animal and persist it on the back end (create)
+// What do I need to do?????
+// - add an action creator - createAnimal <-- should be asynchronous -- returns a function that receives dispatch() as an argument
+//   - ^^ this is where we will add a fetch request and persist to the db
+// - add another action creator to actually update the Redux store once the server reponds appropriately
+// -
+
 class App extends React.Component {
 
   constructor(props){
     super(props)
-    // this.state = {
-    //   animals: []
-    // }
+    this.state = {
+      animals: []
+    }
   }
 
   componentDidMount() {
-    // mocking a GET request
-    // this.setState({
-    //   animals
-    // })
-
     // get my animals
     this.props.getAnimals()
   }
@@ -26,9 +28,9 @@ class App extends React.Component {
   createAnimal = animal => {
     // mocking a POST request
     console.log("create animal")
-    // this.setState({
-    //   animals: animals.concat(animal)
-    // })
+    this.setState({
+      animals: this.state.animals.concat(animal)
+    })
   }
 
   render() {
